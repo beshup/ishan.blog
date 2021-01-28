e1b3fc83f9ccdfb490c3486d0db7e79902e481db
beshup <ishanghimire9@yahoo.ca>
2021-01-28 02:40:04 -0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
       <div class="list">a pretty massive one piece analysis that eventually devolves into something entirely different</div>
       <div class="list">The value of an untrained eye - how user experience at the Air Force and Kanye's experimental ventures oddly taught me about this concept</div>
       <div class="list">My favourite fantasy basketball players ever</div>
-      <div class="list">How I the loss of competitive sports in my life unexpectedly impacted me</div>
+      <div class="list">How the loss of competitive sports in my life unexpectedly impacted me</div>
     </ul>
   </div>
 </template>
