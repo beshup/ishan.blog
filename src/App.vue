8f1e91f5ab352600a04eb8838f672b5c24cd7c5d
beshup <ishanghimire9@yahoo.ca>
2021-01-28 02:24:41 -0800

    first comm

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>gimme a sec on this...........</div>  
+    <div class="header">I have some pieces cooking, here's what you can expect</div>
+    <ul>
+      <div class="list">crypto + sports betting</div>
+      <div class="list">"the ring of fire of tech," my prediction about a global movement</div>
+      <div class="list">discography rankings</div>
+      <div class="list">Book of Five Rings review</div>
+      <div class="list">Deep Dive into Atrocity Exhibition</div>
+      <div class="list">a pretty massive one piece analysis that eventually devolves into something entirely different</div>
+      <div class="list">The value of an untrained eye - how user experience at the Air Force and Kanye's experimental ventures oddly taught me about this concept</div>
+      <div class="list">My favourite fantasy basketball players ever</div>
+      <div class="list">How I felt the loss of competitive sports in my life</div>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Fira Mono', Consolas, Menlo, Monaco, 'Courier New', Courier, monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.header{
+  margin-bottom: 50px;
+}
+
+.list{
+  margin-bottom: 7px;
 }
 </style>
