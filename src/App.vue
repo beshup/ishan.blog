8d51ffe7634635c9113428d61a4417cf8f76ab7a
beshup <ishanghimire9@yahoo.ca>
2021-02-14 22:59:58 -0800

    re-arranging

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@
     <div> 
       <div class="list">crypto + sports betting</div>
       <div class="list">"the tech ring of fire" my prediction about a global movement</div>
-      <div class="list">discography rankings</div>
       <div class="list">a martial artist + nepali take on the book of five rings</div>
       <div class="list">deep dive into atrocity exhibition</div>
+      <div class="list">lessons on service, leadership and their interconnectedness</div>
       <div class="list">a one piece analysis that devolves into something entirely different</div>
+      <div class="list">the story of decentralization: knowledge -> services -> power and why the 3rd stage will completely alter work culture</div>
       <div class="list">the value of an untrained eye - how considering user experience at the air force and kanye's experimental ventures oddly taught me about this concept</div>
       <div class="list">my favourite fantasy basketball players ever</div>
       <div class="list">how the loss of competitive sports in my life unexpectedly impacted me</div>
-      <div class="list">lessons on service, leadership and their interconnectedness</div>
       <div class="list">learning to become opinionated in the creative process</div>
     </div>
   </div>
