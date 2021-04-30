411a72421ee61375fa68a46d32ccd431ce513028
Ishan Ghimire <ishan.ghimire@shopify.com>
2021-04-30 02:30:26 -0700

    cooking up sum more

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
     <div>gimme a sec on this, things are a little hectic right now...........</div>  
-    <div class="header">I have some pieces cooking, here's what you can expect</div>
+    <div class="header">I have some pieces cooking, here's what you can expect.</div>
     <div> 
-      <div class="list">crypto + sports betting</div>
+      <div class="list">ppl love talking abt "intrinsic value" -> let's really break this down</div>
+      <div class="list">cross platform integrations are the next big thing -> here's how the world's biggest db (3th3r3um) will power this</div>
       <div class="list">"the tech ring of fire" my prediction about a global movement</div>
       <div class="list">a martial artist + nepali take on the book of five rings</div>
       <div class="list">deep dive into atrocity exhibition</div>
       <div class="list">lessons on service, leadership and their interconnectedness</div>
+      <div class="list">crypto + sports betting</div>
       <div class="list">a one piece analysis that devolves into something entirely different</div>
       <div class="list">the story of decentralization: knowledge -> services -> power and why the 3rd stage will completely alter work culture</div>
       <div class="list">value of the untrained eye</div>
